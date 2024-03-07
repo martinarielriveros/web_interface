f24edbe130be77eda098ca80993ae8e3c9ae0882
martinarielriveros <martin.riveros@hotmail.com>
2024-03-07 19:16:35 -0300

    mejoras esteticas home

--- web_interface/Main.py
+++ web_interface/Main.py
@@ -14,11 +14,12 @@ from google.cloud import storage
 from google.oauth2 import service_account
 from google.cloud import bigquery
 from st_on_hover_tabs import on_hover_tabs
+from flask import request
 
 st.set_page_config(
     layout="wide",
     page_title="Datavision",
-    page_icon=":rocket:",
+    page_icon="logo_full.png",
     initial_sidebar_state="expanded"
 )
 
@@ -29,8 +30,9 @@ SLACK_API_TOKEN = os.getenv('SLACK_API_TOKEN')
 SLACK_API_TOKEN_BOT = os.getenv('SLACK_API_TOKEN_BOT')
 SLACK_CHANNEL_ID = os.getenv('SLACK_CHANNEL_ID')
 
-def send_slack_notification():
-    
+def send_slack_notification(client_ip):
+
+
     # Get the hostname of the machine
     host_name = socket.gethostname()
 
@@ -38,8 +40,8 @@ def send_slack_notification():
     host_ip = socket.gethostbyname(host_name)
 
     client = WebClient(token=SLACK_API_TOKEN_BOT)
-    message = f'Consulta generada desde IP: {host_ip}'
-
+    message = f'Consulta generada desde IP: {host_ip} & {client_ip}'
+    
     try:
         response = client.chat_postMessage(
             channel=SLACK_CHANNEL_ID,
@@ -49,23 +51,19 @@ def send_slack_notification():
     except SlackApiError as e:
         print(f"Error sending message", e)
 
-
-
 def KNN_model_predict(params, slider_count):
-
-
     
-    # key_file_path = '/final_app/GCP_service_account/key_storage_ML.json'
+
     key_file_path = './GCP_service_account/key_storage_ML.json'
     bucket_name= 'data_clear_ml'
     file_name = 'modelo1_knn.pkl'
 
     # Set up authentication with service account key file
 
-
     storage_client = storage.Client.from_service_account_json(key_file_path)
 
     # Download the file from the bucket
+
     try:
         bucket = storage_client.bucket(bucket_name)
 
@@ -86,7 +84,7 @@ def KNN_model_predict(params, slider_count):
 def get_business_locations(indexes):
 
 
-            # Also, in account service, you need to create the following permissions
+            # Also, in account service, set the following permissions
             # BigQuery Data Viewer
             # BigQuery User
 
@@ -129,15 +127,6 @@ def create_blank_map():
 
     return st_data
 
-def render_ML_graphic():
-        
-        data = {'lat': [-33, -33.1, -33.2], 'lon': [-58.8, -58.6, -58.7], 'size': [1000,2000,3000]}
-        chart_data = pd.DataFrame(data)
-        st.map(chart_data, size='size', zoom=8, use_container_width=True)
-
-def render_ML_data():
-                    pass
-
 st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
 
 
@@ -145,12 +134,10 @@ with st.sidebar:
     tabs = on_hover_tabs(tabName=['Home', 'Dashboard', 'ML'], 
                          iconName=['home', 'dashboard', 'search'], default_choice=0)
 
-
 if tabs=="Home":
-
-            st.header("Datavision Solutions \U0001F3E0", divider='rainbow',anchor=False)
-
-
+            
+            st.image('images/logo_full.png', use_column_width=True, output_format="auto")
+            
             st.subheader('Bienvenidos:')
 
             intro = '''    Somos *Datavision Solutions*, una consultora encargada de ofrecer soluciones avanzadas basadas en datos y les damos la bienvenida a nuestra plataforma en línea, presentando un innovador modelo de predicción, resultado de un minucioso análisis de mercado enfocado en la industria de comida rápida en los Estados Unidos.
@@ -158,15 +145,59 @@ if tabs=="Home":
             Además tendrán la oportunidad de sumergirse en un nuestro dashboard, el cual proporciona un análisis visual profundo de los negocios de comida rápida y su posición en el dinámico mercado estadounidense junto con los Kpis propuestos ofreciendo insights valiosos sobre la interacción y la capacidad de adaptación de cada emprendimiento   
             '''
             st.markdown(intro)
+            
+            ## Nuestra información
+            
+            personas = [
+                {
+                    "nombre": "Belén Viglioglia Becker",
+                    "profesion": "Data Analyst ",
+                    "github": "https://github.com/belenvbecker",
+                    "linkedin": "https://www.linkedin.com/in/belen-viglioglia-becker/",
+                    "imagen_link":"images/belen.jfif"
+                },
+                {
+                    "nombre": "Mariano Popov",
+                    "profesion": "Data Engineer",
+                    "github": "https://github.com/marianopopov",
+                    "linkedin": "https://www.linkedin.com/in/mariano-popov-3a4570290/",
+                    "imagen_link":"images/mariano.jfif"
+                    
+                },
+                {
+                    "nombre": "Marcelo Ortiz",
+                    "profesion": "Data Analyst",
+                    "github": "https://github.com/marceloortizz",
+                    "linkedin": "https://www.linkedin.com/in/marceloortizz/",
+                    "imagen_link":"images/marcelo.jfif"
+                },
+                {
+                    "nombre": "Martín Riveros",
+                    "profesion": "Data Scientist",
+                    "github": "https://github.com/martinarielriveros",
+                    "linkedin": "https://www.linkedin.com/in/martinriveros/",
+                    "imagen_link":"images/martin.jfif"
+                },
+                                {
+                    "nombre": "	Alejandro Ramírez",
+                    "profesion": "Data Engineer",
+                    "github": "https://github.com/dalejandroramirez",
+                    "linkedin": "https://www.linkedin.com/in/dalejandroramirez/",
+                    "imagen_link":"images/alejandro.jfif"
+                }
+            ]
 
-            st.subheader('Autores:')
-            authors = '''   **Belen Viglioglia Becker** - Data Analyst  
-                            **Marcelo Ortiz** - Data Analyst   
-                            **Mariano Popov** - Data Engineer  
-                            **Alejandro Ramirez** - Data Engineer  
-                            **Martin Riveros** - Data Scientist 
-        '''
-            st.markdown(authors)
+            # Mostrar todas las tarjetas en la misma fila
+            column1, column2, column3, column4,column5 = st.columns(5)
+            
+
+            for idx, persona in enumerate(personas):
+                with eval(f"column{idx + 1}"):
+                    st.write(f"### {persona['nombre']}")
+                    st.image(persona['imagen_link'], width=200)
+                    st.write(f"**Profesión:** {persona['profesion']}")
+                    st.write(f"GitHub: [{persona['nombre']}]({persona['github']})")
+                    st.write(f"LinkedIn: [{persona['nombre']}]({persona['linkedin']})")
 
 elif tabs=="Dashboard":
 
@@ -241,7 +272,7 @@ elif tabs=="ML":
                             
             if submitted:
                         
-                        send_slack_notification()
+                        send_slack_notification('Enmascarado por GCP')
                         
                         if st.session_state.keys()=='':
                                 st.warning('Especifique un punto de interes en el plano', icon="⚠️")
